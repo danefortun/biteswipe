ab1d25e4ce1e777889ef5af9aa6e69f15a7c964f
di326 <di326@drexel.edu>
2025-11-10 14:11:40 -0500

    cards are now swipeable and I have cleaned up some of the docs

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let currentIndex = 0;
-    const cards = document.getElementById('cards');
-    const totalCards = document.querySelectorAll('.card').length;
+const cards = document.getElementById('cards');
+const totalCards = document.querySelectorAll('.card').length;
 
-    function swipeLeft() {
-      currentIndex = (currentIndex - 1 + totalCards) % totalCards;
-      updateCardPosition();
-    }
+function swipeLeft() {
+  currentIndex = (currentIndex - 1 + totalCards) % totalCards;
+  updateCardPosition();
+}
 
-    function swipeRight() {
-      currentIndex = (currentIndex + 1) % totalCards;
-      updateCardPosition();
-      console.log(1)
-    }
+function swipeRight() {
+  currentIndex = (currentIndex + 1) % totalCards;
+  updateCardPosition();
+}
 
-    function updateCardPosition() {
-      cards.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
+function updateCardPosition() {
+  cards.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
