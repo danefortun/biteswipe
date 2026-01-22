e573884e6746d9361c38974488fa4e31d2250d2c
Dean Iyoun <di326@drexel.edu>
2026-01-22 14:32:39 -0500

    Added the user class and created an empty database

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from flask import Flask, redirect, url_for, render_template, request,session,flash
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id",db.Integer,primary_key = True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    def __init__(self,name,password,email):
+        self.__name = name
+        self.__email = email
+        self.__password = password
 
 @app.route("/index")
 @app.route("/")
@@ -20,6 +35,7 @@ def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    
+    with app.app_context():
+        db.create_all()
         
     app.run(debug = True)
